046e145dad9aeb9d433bf8e7ed717506a58c909b
Floyd Jones <floydtjones@gmail.com>
2018-01-21 21:59:43 -0600

    a bit of styling and storing relevant information from search in items array

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 $(() => {
+  const items = [];
+
+  search = query => {
+    const params = {
+      part: 'snippet',
+      key: 'AIzaSyBxNr2OGUF-GeC2byGuWkWU-RUvpmIxiZI',
+      q: query
+    }
+    $.getJSON('https://www.googleapis.com/youtube/v3/search', params, data => {
+      parseData(data);
+      console.log(items);
+      //renderData()
+    })
+  }
+
+  parseData = data => {
+    data.items.forEach(item => {
+      items.push({
+        url: item.snippet.thumbnails.high.url,
+        title: item.snippet.title,
+        channel: item.snippet.channelTitle
+      })
+
+      console.log(item)
+    })
+  }
+
   handleSubmit = () => {
     $('form').submit(e => {
       e.preventDefault();
-      console.log(e.currentTarget);
+      let val = $('input').val();
+      search(val);
     });
   }
 
