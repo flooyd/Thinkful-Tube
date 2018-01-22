cda2e13dac1fe3dacd3f48135744d8be312e4bfb
Floyd Jones <floydtjones@gmail.com>
2018-01-21 23:08:45 -0600

    all basic functionality and styling complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(() => {
-  const items = [];
+  let items = [];
 
   search = query => {
     const params = {
@@ -9,21 +9,43 @@ $(() => {
     }
     $.getJSON('https://www.googleapis.com/youtube/v3/search', params, data => {
       parseData(data);
-      console.log(items);
-      //renderData()
+      renderItems();
     })
   }
 
   parseData = data => {
+    items = [];
     data.items.forEach(item => {
       items.push({
         url: item.snippet.thumbnails.high.url,
         title: item.snippet.title,
-        channel: item.snippet.channelTitle
+        channel: item.snippet.channelTitle,
+        channelId: item.snippet.channelId,
+        videoId: item.id.videoId
       })
-
-      console.log(item)
     })
+  }
+
+  renderItems = () => {
+    const results = $('.results-js');
+    $(results).empty();
+
+    items.forEach(item => {
+      const videoLink = `https://www.youtube.com/watch?v=${item.videoId}`;
+      const channelLink = `https://www.youtube.com/channel/${item.channelId}`
+      $(results).append(
+      `<li>
+        <div class="title">
+          ${item.title}
+        </div>
+        <div>
+          Channel: <a href=${channelLink} target=_blank>${item.channel}</a>
+        </div>
+        <a href=${videoLink} target=_blank><img src=${item.url}></a>
+      </li>`
+      )
+    })
+
   }
 
   handleSubmit = () => {
