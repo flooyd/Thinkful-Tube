857cc27be54e6b645750e4ac182ce767652c33e5
flooyd <floydtjones@gmail.com>
2018-01-24 18:38:49 +0000

    modal for playing video complete

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ $(() => {
       part: 'snippet',
       key: 'AIzaSyBxNr2OGUF-GeC2byGuWkWU-RUvpmIxiZI',
       q: query
-    }
-    $.getJSON('https://www.googleapis.com/youtube/v3/search', params, data => {
+    };
+    $.getJSON('https://www.googleapis.com/youtube/v3/search', params)
+    .done(data => {
       parseData(data);
       renderItems();
     })
+    .fail(()=> {
+      console.log('fail')
+    })
+
   }
 
   parseData = data => {
@@ -22,8 +27,8 @@ $(() => {
         channel: item.snippet.channelTitle,
         channelId: item.snippet.channelId,
         videoId: item.id.videoId
-      })
-    })
+      });
+    });
   }
 
   renderItems = () => {
@@ -34,19 +39,35 @@ $(() => {
       const videoLink = `https://www.youtube.com/watch?v=${item.videoId}`;
       const channelLink = `https://www.youtube.com/channel/${item.channelId}`
       $(results).append(
-      `<li>
+        `<li>
         <div class="title">
           ${item.title}
         </div>
         <div>
           Channel: <a href=${channelLink} target=_blank>${item.channel}</a>
         </div>
-        <a href=${videoLink} target=_blank><img src=${item.url}></a>
+        <img id=${item.videoId} src=${item.url}>
       </li>`
-      )
-    })
+      );
+    });
+  };
 
+  renderModal = videoId => {
+    let src = `https://www.youtube.com/embed/${videoId}?rel=0&autoplay=0`;
+    $('.video').empty().append(
+      `
+      <iframe height="100%" width="100%" src=${src} frameborder="0" allowfullscreen></iframe>
+      `
+    );
+    $('.videoModal-js').css('display', 'block');
   }
+
+  handleMainClick = () => {
+    $('.videoModal-js').click(e => {
+      $('iframe').prop('src', '');
+      $('.videoModal-js').css('display', 'none');
+    });
+  };
 
   handleSubmit = () => {
     $('form').submit(e => {
@@ -54,7 +75,16 @@ $(() => {
       let val = $('input').val();
       search(val);
     });
-  }
+  };
+
+  handleVideoClicked = () => {
+    $('.results-js').on('click', 'img', e => {
+      let videoId = $(e.currentTarget).prop('id');
+      renderModal(videoId)
+    });
+  };
 
   handleSubmit();
+  handleVideoClicked();
+  handleMainClick();
 })
